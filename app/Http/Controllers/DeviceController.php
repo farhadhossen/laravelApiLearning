03b7d3dab35df349a256787c2c6cbf3fd35a9faf
farhadhossen <imfarhad01@gmail.com>
2022-01-29 15:39:46 +0600

    form data update

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -26,4 +26,26 @@ class DeviceController extends Controller
         
     }
 
+
+    function addDevice(Request $request)
+    {
+        if($request->ismethod('post')){
+            $data = $request->all();
+
+
+            $device = new Device();
+            $device->name = $data['name'];
+            $device->member_id = $data['member_id'];
+            $result = $device -> save();
+
+            if($result){
+                return ["Result" => "Data has been saved"];
+            }
+            else{
+                return ["Result" => "Data has not been saved"];
+            }
+
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("add",[DeviceController::class,'add']);
+// Route::post("add",[DeviceController::class,'add']);
+
+Route::post("add",[DeviceController::class,'addDevice']);
+
+
 
